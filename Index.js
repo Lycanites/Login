26c9ef0f72d19f57411d7f9e9c3bebcdfd5eb625
Andre_XPG <Lycanites@outlook.com>
2025-04-11 12:07:04 -0600

    Login listo

--- Index.js
+++ Index.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3001;
 
 // Middleware
